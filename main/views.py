470726f66e47e390cd82c85e21b489f6dfeedbd6
mikolaj200293 <mikolaj200293@gmail.com>
2022-01-17 18:01:08 +0100

    Add preliminary configured templates and static

--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import View
+from django.template.response import TemplateResponse
 
+
+class LandingPage(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'base.html')
+
+
+class AddDonation(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'form.html')
+
+
+class Login(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'login.html')
+
+
+class Register(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'register.html')
 # Create your views here.
--- portfoliolab/urls.py
+++ portfoliolab/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import LandingPage, AddDonation, Login, Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPage.as_view(), name='home'),
+    path('add_donation', AddDonation.as_view(), name='add-donation'),
+    path('login', Login.as_view(), name='login'),
+    path('register', Register.as_view(), name='register'),
 ]
