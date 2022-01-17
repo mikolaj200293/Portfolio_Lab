c8d36494eca1eba5688bbad65f45d0cff056a751
mikolaj200293 <mikolaj200293@gmail.com>
2022-01-17 18:26:23 +0100

    Add Institution  model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+INSTITUTIONS = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna'),
+)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Kategoria')
@@ -7,4 +13,13 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
+class Institution(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Instytucja')
+    description = models.TextField(verbose_name="Opis")
+    type = models.IntegerField(choices=INSTITUTIONS, verbose_name="Typ")
+    categories = models.ManyToManyField(Category, verbose_name='Kategoria')
+
+    def __str__(self):
+        return f"{self.name}"
 # Create your models here.
