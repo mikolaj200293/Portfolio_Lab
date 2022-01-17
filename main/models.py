fb794ea2020f0014623307397bad76870037cae7
mikolaj200293 <mikolaj200293@gmail.com>
2022-01-17 18:03:06 +0100

    Add Category model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Kategoria')
+
+    def __str__(self):
+        return f"{self.name}"
+
 # Create your models here.
