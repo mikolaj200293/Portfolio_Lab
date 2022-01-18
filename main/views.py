8e8e40fd4c5353c2528e53d057565ca166e01775
mikolaj200293 <mikolaj200293@gmail.com>
2022-01-18 11:44:21 +0100

    Add dynamic statistics, main page navigation a and redirections

--- main/views.py
+++ main/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.views import View
 from django.template.response import TemplateResponse
+from main.models import Category, Institution, Donation
 
 
 class LandingPage(View):
 
     def get(self, request):
-        return TemplateResponse(request, 'base.html')
+        donations = Donation.objects.all()
+        donated_bags = 0
+        for donation in donations:
+            donated_bags += donation.quantity
+        donated_institutions = []
+        for donation in donations:
+            if donation.institution not in donated_institutions:
+                donated_institutions.append(donation.institution)
+        if not donated_institutions:
+            donated_institutions_number = 0
+        else:
+            donated_institutions_number = len(donated_institutions)
+        ctx = {
+            'donated_bags': donated_bags,
+            'donated_institutions': donated_institutions_number
+        }
+        return TemplateResponse(request, 'base.html', ctx)
 
 
 class AddDonation(View):
